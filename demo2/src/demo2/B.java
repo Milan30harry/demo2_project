f3669cca034d234df895c8d3b322863dd4ef050d
milanfireflink <125262491+milanfireflink@users.noreply.github.com>
2023-03-12 13:40:10 +0530

    just check update for b.java

---
+++ demo2/src/demo2/B.java
@@ -0,0 +1,47 @@
+package demo2;
+
+import java.util.Scanner;
+
+public class B implements interface1 
+{ 
+	int a,b,c=0;
+	public void add()
+	{
+		System.out.println("This is code add");
+	}
+	public void sub()
+	{
+		System.out.println("This is sub");
+	}
+	public int add(int a, int b)
+	{
+		c= a+b;
+		System.out.println("Total a and b sum is ="+c);
+		return c;
+	}
+	public int sub(int a, int b)
+	{
+		c= a-b;
+		System.out.println("Total a and b sum is ="+c);
+		return c;
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Please enter the value of a and b");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		B obj=new B();
+		obj.add(a,b);
+		obj.sub(a,b);
+		obj.add();
+		obj.sub();
+		sc.close();
+
+	}
+	
+
+}
